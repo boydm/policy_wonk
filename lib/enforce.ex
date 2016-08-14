5ce0df682448cf88d3adc2fb89b6dd943819779a
Boyd Multerer <boyd@multerer.com>
2016-08-14 02:26:16 -0700

    refactor call_policy and use from authorized?

--- lib/enforce.ex
+++ lib/enforce.ex
@@ -33,76 +33,28 @@ defmodule PolicyWonk.Enforce do
   def call(conn, opts) do
     # get the policy handling modules
     handlers = []
-      |> append_truthy( opts[:policy_handler] )
-      |> append_truthy( conn.private[:phoenix_controller] )
-      |> append_truthy( @config_policies )
-      |> append_truthy( conn.private[:phoenix_router] )
+      |> PolicyWonk.append_truthy( opts[:policy_handler] )
+      |> PolicyWonk.append_truthy( conn.private[:phoenix_controller] )
+      |> PolicyWonk.append_truthy( @config_policies )
+      |> PolicyWonk.append_truthy( conn.private[:phoenix_router] )
     if handlers == [] do
       raise %PolicyError{message: "No policy modules set"}
     end
 
     # Enumerate through all the policies. Fail if any fail
     Enum.reduce_while( opts.policies, conn, fn(policy, acc_conn) ->
-      call_policy( handlers, acc_conn, policy )
+      case PolicyWonk.call_policy( handlers, acc_conn, policy ) do
+        {:ok, conn} ->
+          {:cont, conn}
+        {:err, conn, err_data} ->
+          {:halt, handle_error( handlers, conn, err_data ) }
+      end
     end)
   end # def call
 
-  #----------------------------------------------------------------------------
-  defp call_policy( handlers, conn, policy ) do
-    # try to call the policy on each handler until one that
-    # has the right signature is found
-
-    #find(enumerable, default \\ nil, fun)
-    # use default of :not_found, so I can tell if the function wasn't
-    # found on any of the handlers
-    Enum.find_value(handlers, :not_found, fn(handler) ->
-      case handler do
-        nil -> false  # empty spot in the handler list
-        h ->
-          try do
-            case h.policy( conn, policy ) do
-              :ok ->    {:cont, conn}
-              true ->   {:cont, conn}
-              false ->  {:halt, handle_error( conn, nil, handler )}
-              {:ok, policy_conn = %Plug.Conn{}} ->
-                {:halt, {:cont, policy_conn}}
-              {:err, err_data} ->
-                {:halt, handle_error( handlers, conn, err_data ) }
-              _ -> raise "malformed policy response"
-            end
-          rescue
-            _e in UndefinedFunctionError ->
-              # tried to call policy, but the function wasn't defined
-              # on the handler. Return false so that find_value will
-              # try the next handler in the list
-              false
-            _e in FunctionClauseError -> false
-            e ->
-              # some other error. let it raise
-              raise e
-          end
-      end
-    end)
-    |> case do
-      :not_found ->
-        # The policy wasn't found on any handler. raise an error
-        msg = "#{IO.ANSI.red}Unable find to a #{IO.ANSI.yellow}policy#{IO.ANSI.red} definition for:\n" <>
-          "#{IO.ANSI.green}Policy: #{IO.ANSI.yellow}#{inspect(policy)}\n" <>
-          "#{IO.ANSI.green}In any of the following modules...#{IO.ANSI.yellow}\n" <>
-          Enum.reduce(handlers, "", fn(h, acc) ->
-            case h do
-              nil -> acc
-              mod -> acc <> inspect(mod) <> "\n"
-            end
-          end) <>
-          IO.ANSI.red
-        raise %PolicyError{ message: msg }
-      response -> response
-    end
-  end
 
   #----------------------------------------------------------------------------
-  # similar to call_policy
+  # similar to PolicyWonk.call_policy
   defp handle_error( handlers, conn, err_data ) do
     Enum.find_value(handlers, :not_found, fn(handler) ->
       case handler do
@@ -145,15 +97,6 @@ defmodule PolicyWonk.Enforce do
         # The policy wasn't found on any handler. raise an error
         raise "policy not found on any handlers"
       response -> response
-    end
-  end
-
-  #----------------------------------------------------------------------------
-  defp append_truthy(list, element) when is_list(list) do
-    cond do
-      is_list(element) -> list ++ element
-      element -> list ++ [element]
-      true -> list
     end
   end
 
--- lib/policy_wonk.ex
+++ lib/policy_wonk.ex
@@ -1,12 +1,22 @@
 defmodule PolicyWonk do
+  
+  # define a policy error here - not found or something like that
+  defmodule PolicyError do
+    defexception [message: "#{IO.ANSI.red}Unable to execute a policy\n"]
+  end
 
   #===========================================================================
   defmacro __using__(_opts) do
     quote do
-      
+      @policy_wonk_policies Application.get_env(:policy_wonk, PolicyWonk)[:policy_modules]
+
       #------------------------------------------------------------------------
       def authorized?(conn, action_or_name) do
-        case policy(conn, action_or_name) do
+        handlers = [ __MODULE__ ]
+          |> PolicyWonk.append_truthy( @policy_wonk_policies )
+          |> PolicyWonk.append_truthy( conn.private[:phoenix_router] )
+  #      IO.puts "~~~~~~~~~~~~> #{inspect(handlers)}"
+        case PolicyWonk.call_policy(handlers, conn, action_or_name) do
           :ok ->        true
           true ->       true
           false ->      false
@@ -18,5 +28,69 @@ defmodule PolicyWonk do
       
     end # quote
   end # defmacro
+
+  #----------------------------------------------------------------------------
+  def call_policy( handlers, conn, policy ) do
+    # try to call the policy on each handler until one that
+    # has the right signature is found
+
+    #find(enumerable, default \\ nil, fun)
+    # use default of :not_found, so I can tell if the function wasn't
+    # found on any of the handlers
+    Enum.find_value(handlers, :not_found, fn(handler) ->
+      case handler do
+        nil -> false  # empty spot in the handler list
+        h ->
+          try do
+            case h.policy( conn, policy ) do
+              :ok ->    {:ok, conn}
+              true ->   {:ok, conn}
+              false ->  {:err, conn, nil }
+              {:ok, policy_conn = %Plug.Conn{}} ->
+                {:ok, policy_conn}
+              {:err, err_data} ->
+                {:err, conn, err_data }
+              _ -> raise "malformed policy response"
+            end
+          rescue
+            _e in UndefinedFunctionError ->
+              # tried to call policy, but the function wasn't defined
+              # on the handler. Return false so that find_value will
+              # try the next handler in the list
+              false
+            _e in FunctionClauseError -> false
+            e ->
+              # some other error. let it raise
+              raise e
+          end
+      end
+    end)
+    |> case do
+      :not_found ->
+        # The policy wasn't found on any handler. raise an error
+        msg = "#{IO.ANSI.red}Unable find to a #{IO.ANSI.yellow}policy#{IO.ANSI.red} definition for:\n" <>
+          "#{IO.ANSI.green}Policy: #{IO.ANSI.yellow}#{inspect(policy)}\n" <>
+          "#{IO.ANSI.green}In any of the following modules...#{IO.ANSI.yellow}\n" <>
+          Enum.reduce(handlers, "", fn(h, acc) ->
+            case h do
+              nil -> acc
+              mod -> acc <> inspect(mod) <> "\n"
+            end
+          end) <>
+          IO.ANSI.red
+        raise %PolicyError{ message: msg }
+      response -> response
+    end
+  end
+
+  #----------------------------------------------------------------------------
+  def append_truthy(list, element) when is_list(list) do
+    cond do
+      is_list(element) -> list ++ element
+      element -> list ++ [element]
+      true -> list
+    end
+  end
+
 
 end

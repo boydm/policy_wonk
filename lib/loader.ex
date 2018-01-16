fb7d1e7eb5e63b4ad2cf3722db36b40cb788b7cd
Boyd Multerer <boyd@multerer.com>
2018-01-15 22:51:59 -0800

    re-write docs

--- lib/loader.ex
+++ lib/loader.ex
@@ -1,30 +1,142 @@
 defmodule PolicyWonk.Loader do
   @moduledoc """
 
-  To keep resource loading logic organized, PolicyWonk uses `load_resource` functions that you create either in your controllers, router, or a central location.
+  # Overview
+  
+  A resource loader is a function that loads (or prepares) a single resource. The result is put
+  into the conn's assigns field.
 
-  A `load_resource` function attempts to load a given resource into memory and returns that loaded resource
+    A simple resource loader:
 
       def load_resource( _conn, :user, %{"id" => user_id} ) do
-        case Repo.get(Account.User, user_id) do
-          nil ->  "User not found"
+        case MyAppWeb.Account.get_user(user_id) do
+          nil ->  {:error, :not_found}
           user -> {:ok, :user, user}
         end
       end
+
+  The above loader takes the `user_id` (which is from the params of the current request) and
+  attemps to load a user model from the database. The result is put into the conn's assigns field
+  under the `:user` key.
+
+  Your loaders don't need to hit a database. You could generate data or otherwise prepare something
+  else. The point is that the result gets put into the conn's assigns map.
+
+  ## Usage
+
+  The only time you should directly use the `PolicyWonk.Loader` module is to call
+  `use PolicyWonk.Loader` when defining your resource loader module.
+
+  `use PolicyWonk.Loader` injects the `load/3`, `load!/3`, functions into your loader modules.
+  These run and evaluate your load_resource functions and act accordingly on the results.
+
+  Loading resources during the plug chain has several important benefits:
+
+  * When a resource can't be loaded, you can halt the plug chain and handle the error before your
+  actions get called. This lets you code your actions for the happy path.
+  * Reuse and consistency improves in how resources are loaded. You can write one loader that
+  is accessed by many controllers or router pipelines.
+  * It lets you enforce policies on the resources before your actions are called. The best way to
+  avoid security mistakes is to never run that code in the first place.
    
-  The *only* way to indicate success from a `load_resource` function is to return a tuple such as `{:ok, :name, loaded_resource}`. The first field must be `:ok` to indicate success. The middle field is the name of the resource as you want it assigned into `conn.assigns` and must be an atom. The last field is the resource itself.
+  The *only* way to indicate success from a `load_resource` function is to return a tuple such as
+  `{:ok, key, resource}`. The first field in the tuple must be `:ok` to indicate success. The middle
+  field is the name of the resource as you want it assigned into `conn.assigns`. The last field
+  is the resource itself.
 
-  The idea is that you define multiple `load_resource` functions and use Elixir’s pattern matching to find the right one. Like policies, this loader name could be an atom, tuple, map or really anything Elixir can match against.
+  The idea is that you define multiple `load_resource` functions and use Elixir’s pattern matching to
+  find the right one. Like policies, this loader name could be an atom, tuple, map or really
+  anything Elixir/Erlang can match against.
 
-  The first parameter is the current `%Plug.Conn{}` and the last is the conn’s `params` field. You are not expected to directly manipulate the conn here. Instead, the plug adds your resource to the conn’s assigns field for you. See [Synchronous vs. Asynchronous Loading](#module-synchronous-vs-asynchronous-loading) for more information.
+  If the resource fails to load, return {:error, message}, which will in turn pass the message term
+  to your loader_error callback.
+
+  In general, if a requested resource fails to load, it halts the plug and handles the error before
+  the request controller action is ever run. This front-loads the resource loading checks before
+  the controller/actions using router pipelines as a choke point.
+
+
+  Example resource loader module:
+
+        defmodule MyAppWeb.Loaders do
+          use PolicyWonk.Loader           # set up support for resources
+          use PolicyWonk.LoadResource     # turn this module into an resource loading into a plug
+
+          def load_resource( _conn, :user, %{"id" => user_id} ) do
+            case MyAppWeb.Account.get_user(user_id) do
+              nil ->  {:error, :not_found}
+              user -> {:ok, :user, user}
+            end
+          end
+
+          def policy_error(conn, :not_found) do
+            MyAppWeb.ErrorHandlers.resource_not_found(conn, "Resource Not Found")
+          end
+        end
+
+
+  ## Injected functions
+
+  When you call `use PolicyWonk.Loader`, the following functions are injected into your module.
+
+  ### load/3
+
+  `load(conn, resource, async \\ false)`
+
+  Callable as a local plug. Load accepts the current conn and a resource indicator.
+  It then calls the load_resource function, evaluates the response and either
+  puts the result into conn.assigns or transforms the conn with a failure.
+
+  You will normally only use this function if you want to enforce a policy that is
+  written into a controller. Then the plug call will look like this:
+
+        plug :load, :some_resource
+
+
+  If you want to enforce a policy from your router, please read the `PolicyWonk.LoadResource`
+  documentation.
+
+  parameters:
+
+  * `conn` The current conn in the plug chain
+  * `resource` The resource or resources you want to load. This can be either a single
+  term representing one resource, or a list of resource terms.
+  * 'async' a true/false flag indicating if the resources passed in a list should be
+  loaded asynchronously or not.
+
+
+  ### load!/3
+
+  `load!(conn, resource, async \\ false)`
+
+  Loads a resource and returns it. Raises the load_resource function returns {:error, message}.
+  This is a handy way to use your load_resource functions from within an action in a controller.
+
+  If multiple resources are requested the loaded resources are returned in a list of tuples
+  indicating which resources are which.
+
+
+        load!(conn, [:user, :thing])
+        # returns something like...
+        [{:user, user}, {:thing, thing}]
+
+  parameters:
+
+  * `conn` The current conn in the plug chain
+  * `resource` The resource or resources you want to load. This can be either a single
+  term representing one resource, or a list of resource terms.
+  * 'async' a true/false flag indicating if the resources passed in a list should be
+  loaded asynchronously or not.
 
   ## Loader Failures
 
-  To gracefully handle a load error, return anything *other* than the `{:ok, :name, resource}` success tuple. This data will be passed into your `load_error` function.
+  To gracefully handle a load error, return a `{:error, message}` tuple.
 
-  `PolicyWonk.LoadResource` will then cease attempting to load other resources and call your `load_error(conn, error_data)` function. The `error_data` parameter is what you returned from your `load_resource` function.
+  `PolicyWonk.LoadResource` will then cease attempting to load other resources and call your
+  `load_error(conn, message)` function. The `message` parameter is what you returned from your
+  `load_resource` function.
 
-      def load_error(conn, err_data) do
+      def load_error(conn, message) do
         conn
         |> put_status(404)
         |> put_view(MyApp.ErrorView)
@@ -32,37 +144,38 @@ defmodule PolicyWonk.Loader do
         |> halt()
       end
 
-  The `load_error` function works just like a regular plug function. It takes a `conn`, and whatever was returned from the loader. You can manipulate the `conn` however you want to respond to that error. Then return the `conn`.
+  The `load_error` function works just like a regular plug function. It takes a `conn`, and
+  whatever was returned from the loader. You can manipulate the `conn` however you want to
+  respond to that error. Then return the `conn`.
 
-  Unlike handling a policy error, `halt(conn)` is not called for you. If you want the resource load failure to halt the plug chain, make sure to call `halt(conn)` in your load_error function.
+  Unlike handling a policy error, `halt(conn)` is not called for you. If you want the
+  resource load failure to halt the plug chain, make sure to call `halt(conn)` in your
+  load_error function.
 
-  ## Synchronous vs. Asynchronous Loading
+  Sometimes you want the plug chain to continue with a nil resource...
 
-  When you invoke the `PolicyWonk.LoadResource` plug, you can pass in a single resource definition or a list of them.
+  ## Use outside the plug chain
 
-      plug PolicyWonk.LoadResource, [:thing1, :thing2]
+  Resources are usually loaded through a plug, but can also be used inside of other code, such
+  as an action. If you are just reading from a db, then you should probably call your model context
+  functions instead. But if it does something more complicated to prepare a resource, then this
+  can be pretty handy.
 
+  In an action in a controller:
 
-  If you set `load_async` to `true` in the `config` parameters, or pass it in directly, then `PolicyWonk.LoadResource` will attempt to load all the resources in the list at the same time.
+        def settings(conn, params) do
+          ...
+          # raise an error if the resource fails to load.
+          resource = MyAppWeb.Loaders.load!(conn, :some_resource)
+          ...
+        end
 
-  Coordinating these loads is why your `load_resource` function returns the resource in a tuple instead of directly adding it to the conn’s assign field.
-
-  If you need the fields loaded synchronously, either set the flag to false, or load the resources in separate plug invocations.
-
-      plug PolicyWonk.LoadResource, :thing1
-      plug PolicyWonk.LoadResource, :thing2
 
   ## Loader Locations
 
-  When a `load_resource` function is used in a single controller, then it should be defined on that controller. Same for the router. 
-
-  If a `load_resource` function is used in multiple locations, then you should define it in a central loaders file that you refer to in your configuration data.
-
-  In general, when you invoke the `PolicyWonk.LoadResource` plug, it detects if the incoming `conn` is being processed by a Phoenix controller or router. It looks in the appropriate controller or router for a matching `load_resource` function first. If it doesn’t find one, it then looks in loader module specified in the configuration block.
-
-  This creates a form of loader inheritance/polymorphism. The controller (or router) calling the plug always has the authoritative say in what `load_resource` function to use.
-
-  You can also specify the loader’s module when you invoke the `PolicyWonk.LoadResource` plug. This will be the only module the plug looks for a `load_resource` function in.
+  You can build as many Loader modules as you want in multiple umbrella applications. Simply
+  call `use PolicyWonk.Loader` to add the support functions to your module. Call `use PolicyWonk.LoadResource`
+  to make your module a plug that can be called in the router.
 
   """
 
@@ -72,37 +185,44 @@ defmodule PolicyWonk.Loader do
   ## parameters
   * `conn`, the current conn in the plug chain. For informational purposes.
   * `resource`, The resource term you requested with invoking the plug
-  * `params`, the `params` field from the current `conn`. Passed in as a convenience. Useful for parsing and matching against.
-          def load_resource( _conn, :user, %{"id" => user_id} ) do
-            case Repo.get(Account.User, user_id) do
-              nil ->  "User not found"
-              user -> {:ok, :user, user}
-            end
-          end
+  * `params`, the `params` field from the current `conn`. Passed in as a convenience. Useful for
+  parsing and matching against.
 
-  ## Returns
-  * `{:ok, :name, resource}` If the load succeeds, return the loaded resource in a tuple with :ok, the resource name as an atom, and the resource itself.
-  * `error_data` If the load fails, return any error data you want
+  ## Return values
+
+  Must return either {:ok, key, resource} or {:error, message}. If it is an error, the message term will be pass on
+  you your load_error callback unchanged.
+
+  Example:
+        def load_resource( _conn, :user, %{"id" => user_id} ) do
+          case Repo.get(Account.User, user_id) do
+            nil ->  {:error, "User not found"}
+            user -> {:ok, :user, user}
+          end
+        end
   """
   @callback load_resource(conn :: Plug.Conn.t(), resource :: any, params :: Map.t()) :: {:ok, atom, any} | {:error, any}
 
   @doc """
-  Handle a load error.
+  Handle a resource load error. Only called during the plug chain.
+
+  Must return a conn, which you are free to transform.
 
   ## parameters
   * `conn`, the current conn in the plug chain. Transform this to handle the error.
   * `error_data`, the `error_data` returned from your load_resource function.
 
-          def load_error(conn, err_data) do
-            conn
-            |> put_status(404)
-            |> put_view(MyApp.ErrorView)
-            |> render("404.html")
-            |> halt()
-          end
+  Example:
+        def load_error(conn, err_data) do
+          conn
+          |> put_status(404)
+          |> put_view(MyApp.ErrorView)
+          |> render("404.html")
+          |> halt()
+        end
     
-  ## Returns
-  * `conn`, return the transformed `conn`, which will be used in the plug chain..
+  Unlike policies, if you want to halt the plug chain on a resource load error, you
+  must call halt() yourself during the load_error function.
   """
   @callback load_error(conn :: Plug.Conn.t(), message :: any) :: Plug.Conn.t()
 
@@ -121,8 +241,53 @@ defmodule PolicyWonk.Loader do
       @behaviour PolicyWonk.Loader
 
       # ----------------------------------------------------
-      def load(conn, resources, async \\ false),
-        do: PolicyWonk.Loader.load(conn, __MODULE__, resources, async)
+      @doc """
+      Callable as a local plug. Loads one or more resources.
+
+      You will normally only use this function if you want to enforce a policy that is
+      written into a controller. Then the plug call will look like this:
+
+            plug :load, :some_resource
+
+
+      If you want to load a resource from your router, please read the `PolicyWonk.LoadResource`
+      documentation.
+
+      ## Parameters
+      * `conn` The current conn in the plug chain
+      * `resoruce` The resource or resources you want to load. This can be either a single
+      term representing one resource, or a list of resource terms.
+      * `async` flag indicating if a list of resources should be loaded asynchronously.
+      """
+      def load(conn, resources, async \\ false) do
+        PolicyWonk.Loader.load(conn, __MODULE__, resources, async)
+      end
+
+      # ----------------------------------------------------
+      @doc """
+      Evaluates one or more resource loaders and returns the results. 
+
+      In an action in a controller:
+
+            def settings(conn, params) do
+              ...
+              # raise an error if the resource fails to load.
+              resource = MyAppWeb.Loaders.load!(conn, :some_resource)
+              ...
+            end
+
+      If multiple resources are requested, they will be returned in a list of tuples.
+
+            MyAppWeb.Loaders.load!(conn, [:thing_a, :thing_b])
+            # would return something like
+            [{:thing_a, thing_a}, {:thing_b, thing_b}]
+
+      ## Parameters
+      * `conn` The current conn in the plug chain
+      * `resoruce` The resource or resources you want to load. This can be either a single
+      term representing one resource, or a list of resource terms.
+      * `async` flag indicating if a list of resources should be loaded asynchronously.
+      """
 
       def load!(conn, resources, async \\ false),
         do: PolicyWonk.Loader.load!(conn, __MODULE__, resources, async)
@@ -156,13 +321,9 @@ defmodule PolicyWonk.Loader do
           {:cont, Plug.Conn.assign(acc_conn, key, resource)}
 
         {:error, message} ->
-          # halt the plug chain
-          acc_conn =
-            acc_conn
-            |> module.load_error(message)
-            |> Plug.Conn.halt()
-
-          {:halt, acc_conn}
+          # handle the error
+          acc_conn = module.load_error(acc_conn, message)
+          {:cont, acc_conn}
       end
     end)
   end
@@ -174,10 +335,8 @@ defmodule PolicyWonk.Loader do
         Plug.Conn.assign(conn, key, resource)
 
       {:error, message} ->
-        # halt the plug chain
-        conn
-        |> module.load_error(message)
-        |> Plug.Conn.halt()
+        # handle the error
+        module.load_error(conn, message)
     end
   end
 
